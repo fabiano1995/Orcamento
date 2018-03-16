c62810d501cae0f96070d077ffb81ab5da048c0b
fabiano1995 <fblourencodeoliveira@gmail.com>
2018-03-16 20:15:03 -0300

    Abrindo Activity

--- app/src/main/java/com/fnoc/orcamento/orcamento/MainActivity.java
+++ app/src/main/java/com/fnoc/orcamento/orcamento/MainActivity.java
@@ -1,13 +1,39 @@
 package com.fnoc.orcamento.orcamento;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button btnPiso;
+    private Button btnParede;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPiso = findViewById(R.id.btnPiso);
+        btnParede = findViewById(R.id.btnParde);
+
+        btnPiso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity( intent );
+            }
+        });
+
+        btnParede.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity( intent );
+            }
+        });
+
     }
 }
